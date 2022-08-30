65f2dab17069e71f59a5a099188b2446be59e76d
schmittaa <schmidtkhaoula@gmail.com>
2022-08-30 20:45:57 +0200

    change server port conf

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = 5000 ;
+const PORT = process.env.PORT || 5000 ;
 const database=require('./config/connexion');
 const authRouter = require('./routes/auth')
 const animeRouter = require('./routes/anime')
