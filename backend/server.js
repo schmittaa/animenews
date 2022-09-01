7bb3fa9a19888a186dbb440940ff8e1fde8795a3
schmittaa <schmidtkhaoula@gmail.com>
2022-09-01 18:39:28 +0200

    make it better

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = 5000 ;
+const PORT = 8080 ;
 const database=require('./config/connexion');
 const authRouter = require('./routes/auth')
 const animeRouter = require('./routes/anime')
